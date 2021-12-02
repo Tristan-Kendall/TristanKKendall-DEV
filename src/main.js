2ea27bd87ebe64b7a95597c186999b940afe7eea
TristanKKendall <tristankk.business@gmail.com>
2021-12-01 18:45:06 -0800

    Look ma, I'm on a website. Can't get image to work

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import './index.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>This is a test</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  <p>This is another test, how do I make a box.
   `
